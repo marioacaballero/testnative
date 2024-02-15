d41955c982a0ebcac57a032d26b10fd619b3d06c
Mario Caballero <mario.caballero.948@gmail.com>
2024-02-15 12:18:29 -0300

    some changes

---
+++ src/Default0.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { style } from './default0Style';
+import { StyleSheet } from 'react-native';
+import { View } from "react-native"; 
+
+export default function Default0() {
+  
+  return (
+  <View style={styles.aythdefaulthykg2f}><StatusBar style="auto"
+  />
+</View>
+
+  );
+};
+const styles = StyleSheet.create(style);
---
+++ src/default0Style.js
@@ -0,0 +1,10 @@
+export const style = {
+  aythdefaulthykg2f: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+  },
+}
